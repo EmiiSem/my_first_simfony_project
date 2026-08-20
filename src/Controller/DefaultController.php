25701baeacd6ba9938fd259a0b8f97be8c01c130
EmiiSem <just_dragon29@mail.ru>
2026-08-20 15:17:26 +0300

    Убрано всё лишнее: запросы на добавления. Сделан редирект на CRUD app_blog_index - главная страница списка блогов

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,9 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Blog;
-use App\Repository\BlogRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,25 +9,8 @@ use Symfony\Component\Routing\Attribute\Route;
 final class DefaultController extends AbstractController
 {
     #[Route('/', name: 'project_default')]
-    public function index(BlogRepository $blogRepository,  EntityManagerInterface $em): Response
+    public function index(): Response
     {
-        $blog = $blogRepository->findOneBy(['id' => 2]);
-        //$em->remove($blog); // remove - запрос на удаление
-        $em->refresh($blog); // refresh - получение свежих данных
-        $em->flush(); // flush - зафексировать изменения
-        dump($blog);
-        exit;
-
-        $blog = (new Blog())
-                ->setTitle('My First Blog')
-                ->setDescription('This is a description.')
-                ->setText('This is a text.')
-        ;
-
-        $em->persist($blog); // persist - запрос на добавление
-        $em->flush(); // flush - зафексировать изменения
-        exit;
-
-        return $this->render('index.html.twig', []);
+        return $this->redirectToRoute('app_blog_index');
     }
 }
