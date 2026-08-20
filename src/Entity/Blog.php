6e4992711b7404a01e464ea348ad3cfb89de47dd
EmiiSem <just_dragon29@mail.ru>
2026-08-20 15:19:54 +0300

    Добавлены гетер и сетер новой сущности Категория, поставлена связь один-ко-многим через category_id, добавлено поле в БД

--- src/Entity/Blog.php
+++ src/Entity/Blog.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use App\Repository\BlogRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Symfony\Component\Mime\Address;
 
 #[ORM\Entity(repositoryClass: BlogRepository::class)]
 class Blog
@@ -22,6 +24,10 @@ class Blog
 
     #[ORM\Column(type: Types::TEXT)]
     private ?string $text = null;
+
+    #[ORM\ManyToOne(targetEntity: Category::class)]
+    #[ORM\JoinColumn(name: 'category_id', referencedColumnName: 'id')]
+    private Category|null $category = null;
 
     public function getId(): ?int
     {
@@ -60,6 +66,18 @@ class Blog
     public function setDescription(string $description): static
     {
         $this->desciption = $description;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): static
+    {
+        $this->category = $category;
 
         return $this;
     }
