a7b073422836eaf34952de1fac79676658ac4a6b
EmiiSem <just_dragon29@mail.ru>
2026-08-21 16:29:01 +0300

    Настроена авторизация: form_login, CSRF, редиректы по ролям и сообщение Недостаточно прав

---
+++ src/Security/LoginSuccessHandler.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+
+class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    public function __construct(
+        private UrlGeneratorInterface $urlGenerator,
+        private AuthorizationCheckerInterface $authorizationChecker,
+    ) {
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): Response
+    {
+        if ($this->authorizationChecker->isGranted('ROLE_ADMIN')) {
+            return new RedirectResponse($this->urlGenerator->generate('app_blog_index'));
+        }
+
+        $request->getSession()->getFlashBag()->add('warning', 'Недостаточно прав');
+
+        return new RedirectResponse($this->urlGenerator->generate('app_login'));
+    }
+}
