34cc2d899f96e384c30f1bf6af16cbe80ef87d2f
EmiiSem <just_dragon29@mail.ru>
2026-08-21 14:25:52 +0300

    В BlogRepository добавлен метод findByBlogFilter для выборки блогов по фильтру заголовка

--- src/Repository/BlogRepository.php
+++ src/Repository/BlogRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Blog;
+use App\Filter\BlogFilter;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -16,5 +17,18 @@ class BlogRepository extends ServiceEntityRepository
         parent::__construct($registry, Blog::class);
     }
 
+    public function findByBlogFilter(BlogFilter $blogFilter)
+    {
+        $blogs = $this->createQueryBuilder('b')->where('1 = 1');
+
+        if($blogFilter->getTitle()) {
+            $blogs
+                ->andWhere('b.title LIKE :title')
+                ->setParameter('title', '%'.$blogFilter->getTitle().'%')
+            ;
+        }
+
+        return $blogs->getQuery()->getResult();
+    }
 
 }
