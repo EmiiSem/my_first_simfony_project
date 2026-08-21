b7520eb90434be404c2fa2bed1f493b403a18ebb
EmiiSem <just_dragon29@mail.ru>
2026-08-21 14:27:09 +0300

    В BlogType уточнены типы полей, валидация required, сортировка категорий и placeholder

--- src/Form/BlogType.php
+++ src/Form/BlogType.php
@@ -5,8 +5,10 @@ namespace App\Form;
 use App\Entity\Blog;
 use App\Entity\Category;
 use App\Form\DataTransformer\TagTransformer;
+use App\Repository\CategoryRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -21,14 +23,25 @@ class BlogType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('text')
+            ->add('title', TextType::class, [
+                'required' => true,
+                'help' => 'Заполните заголовок блога'
+            ])
+            ->add('description', TextareaType::class, [
+                'required' => true,
+            ])
+            ->add('text', TextareaType::class, [
+                'required' => true,
+            ])
             ->add('category', EntityType::class, [
                 'class' => Category::class,
+                'query_builder' => function (CategoryRepository $repository) {
+                    return $repository->createQueryBuilder('p')->orderBy('p.name', 'ASC');
+                },
                 'choice_label' => 'name',
                 'required' => false,
                 'empty_data' => null,
+                'placeholder' => '-- выбор категории --',
             ])
             ->add('tags', TextType::class, [
                 'label' => 'Теги',
