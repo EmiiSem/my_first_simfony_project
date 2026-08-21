168951987f656b73ce44776304d1a28abd61740a
EmiiSem <just_dragon29@mail.ru>
2026-08-21 14:27:48 +0300

    В сущность Blog добавлена валидация NotBlank для title, description и text

--- src/Entity/Blog.php
+++ src/Entity/Blog.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\PersistentCollection;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: BlogRepository::class)]
 class Blog
@@ -17,12 +18,15 @@ class Blog
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Assert\NotBlank(message: 'Заголовок обязателен к заполнению')]
     #[ORM\Column(length: 255)]
     private ?string $title = null;
 
+    #[Assert\NotBlank(message: 'Описание обязательно к заполнению')]
     #[ORM\Column(length: 255)]
     private ?string $desciption = null;
 
+    #[Assert\NotBlank(message: 'Текст обязателен к заполнению')]
     #[ORM\Column(type: Types::TEXT)]
     private ?string $text = null;
 
