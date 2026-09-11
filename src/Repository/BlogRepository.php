e1eae8150ccd16e7a4278ffde0e59d612805dd54
EmiiSem <just_dragon29@mail.ru>
2026-09-11 11:26:40 +0300

    убран лишний use, убран лишний запрос к БД

--- src/Repository/BlogRepository.php
+++ src/Repository/BlogRepository.php
@@ -3,7 +3,6 @@
 namespace App\Repository;
 
 use App\Entity\Blog;
-use App\Entity\User;
 use App\Filter\BlogFilter;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\QueryBuilder;
@@ -31,7 +30,7 @@ class BlogRepository extends ServiceEntityRepository
     public function findByBlogFilter(BlogFilter $blogFilter): QueryBuilder
     {
         $blogs = $this->createQueryBuilder('b')
-            ->leftJoin(User::class, 'u', 'WITH', 'u.id = b.user')
+            ->leftJoin('b.user', 'u')
             ->where('1 = 1')
         ;
 
@@ -48,6 +47,8 @@ class BlogRepository extends ServiceEntityRepository
                 ->setParameter('title', '%'.$blogFilter->getTitle().'%')
             ;
         }
+
+        $blogs->orderBy('b.id', 'DESC');
 
         return $blogs;
     }
